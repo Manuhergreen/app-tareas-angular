bf5c9e992825df09b3268f4d32ec4640911252cb
Manuhergreen <manuhernandezbi@gmail.com>
2023-07-11 13:37:56 +0200

    app tareas

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+//modulos
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+//componentes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TareasComponent } from './components/tareas/tareas.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    TareasComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
